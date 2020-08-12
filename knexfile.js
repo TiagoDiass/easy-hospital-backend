30d0dffa2b3802c58b57458d79d4e05a7f265259
TiagoDiass <tiago.costadiasss@gmail.com>
2020-08-12 00:00:15 -0300

    :rocket: again...

--- knexfile.js
+++ knexfile.js
@@ -21,22 +21,4 @@ module.exports = {
 
     useNullAsDefault: true,
   }
-
-  // development old: {
-  //   client: 'sqlite3',
-  //   connection: {
-  //     filename: './src/database/database.sqlite'
-  //   },
-
-  //   migrations: {
-  //     tableName: 'knex_migrations',
-  //     directory: './src/database/migrations'
-  //   },
-
-  //   seeds: {
-  //     directory: './src/database/seeds'
-  //   },
-
-  //   useNullAsDefault: true
-  // },
 };
