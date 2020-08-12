c2d12de2d45fd07be4ed9091e395b22c9c2b794b
TiagoDiass <tiago.costadiasss@gmail.com>
2020-08-12 12:55:22 -0300

    :rocket: Knexfile configured to deploy

--- knexfile.js
+++ knexfile.js
@@ -2,6 +2,27 @@ require('dotenv').config();
 const path = require('path');
 
 module.exports = {
+  production: {
+    client: process.env.DB_CLIENT || 'pg',
+
+    connection: {
+      host: process.env.PG_HOST,
+      user: process.env.PG_USER,
+      password: process.env.PG_PASSWORD,
+      database: process.env.PG_DATABASE,
+    },
+
+    migrations: {
+      directory: path.resolve(__dirname, 'src', 'database', 'migrations'),
+    },
+
+    seeds: {
+      directory: path.resolve(__dirname, 'src', 'database', 'seeds'),
+    },
+
+    useNullAsDefault: true,
+  },
+
   development: {
     client: process.env.DB_CLIENT || 'pg',
 
